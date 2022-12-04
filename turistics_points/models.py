b07fb6185ac131a3012d7a6fed1dc722a3b2f65a
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2022-12-04 09:26:41 -0300

    feat: created relationship with comments in turistics points

--- turistics_points/models.py
+++ turistics_points/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from equipments.models import Equipment
-
+from comments.models import Comment
 # Create your models here.
 class TuristicPoint(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     approval = models.BooleanField(default=False)
-    equipments = models.ManyToManyField(Equipment, related_name="Turistic_Point")
-    
+    equipments = models.ManyToManyField(Equipment, related_name="Turistic_Point", blank=True)
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="Turistic_Point")    
     def __str__(self) -> str:
         return self.name
+
