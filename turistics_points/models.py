d4e47753f6e3f944d68dc25dc3f08cf45b5893a6
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2022-12-04 17:24:33 -0300

    feat: populate bd with dates and alter in bd

--- turistics_points/models.py
+++ turistics_points/models.py
@@ -8,7 +8,7 @@ class TuristicPoint(models.Model):
     description = models.TextField()
     approval = models.BooleanField(default=False)
     equipments = models.ManyToManyField(Equipment, related_name="Turistic_Point", blank=True)
-    comments = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="Turistic_Point") 
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="Turistic_Point", null=True, blank=True) 
     localizations = models.ForeignKey(Localization, on_delete=models.CASCADE, related_name="Turustuc_Points", null=True, blank=True)   
     
     def __str__(self) -> str:
