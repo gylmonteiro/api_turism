7a9044f36f15b6ea0f1cacb45334634d7f7602e6
gylmonteiro <gyl.tecno.especialista@gmail.com>
2022-12-27 11:37:57 -0300

    feat: created actions

--- turistics_points/views.py
+++ turistics_points/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from .models import TuristicPoint
 from .serializers import TuristicPointSerializer
-# Create your views here.
+from rest_framework.decorators import action
 
 class TuristicPointView(ModelViewSet):
     # queryset = TuristicPoint.objects.all()
@@ -16,3 +16,23 @@ class TuristicPointView(ModelViewSet):
     def create(self, request, *args, **kwargs):
         print (request.data)
         return Response({"teste": "criei recurso"})
+
+    def destroy(self, request, *args, **kwargs):
+        return Response({"Ok": "Delete"})
+    
+    def retrieve(self, request, *args, **kwargs):
+        return Response({"teste": "ok"})
+    
+    def update(self, request, *args, **kwargs):
+        return Response({"teste": "ok"})
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response({"teste": "ok"})
+
+    @action(methods=["get"], detail=False)
+    def denunciar(self, request, pk=None):
+        return Response({"teste": "Cheguei na action"})
+
+    @action(methods=["post", "get"], detail=True)
+    def test(self, request, pk=None):
+        return Response({"teste": "Cheguei na action"})
