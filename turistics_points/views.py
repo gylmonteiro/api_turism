5ec53becda86a6952fbfe306fa47651073af0a5d
gylmonteiro <gyl.tecno.especialista@gmail.com>
2022-12-27 10:42:44 -0300

    feat: subscribe methods list and create

--- turistics_points/views.py
+++ turistics_points/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from .models import TuristicPoint
 from .serializers import TuristicPointSerializer
 # Create your views here.
@@ -8,3 +9,10 @@ class TuristicPointView(ModelViewSet):
     serializer_class = TuristicPointSerializer
     def get_queryset(self):
         return TuristicPoint.objects.filter(approval = True)
+
+    def list(self, request, *args, **kwargs):
+        return Response({"teste": "deu certo"})
+
+    def create(self, request, *args, **kwargs):
+        print (request.data)
+        return Response({"teste": "criei recurso"})
