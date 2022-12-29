b61389e6c417813ccb4f304ae727b528e828e493
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2022-12-29 08:33:46 -0300

    feat: cretaed filter in queryset

--- turistics_points/views.py
+++ turistics_points/views.py
@@ -10,17 +10,31 @@ class TuristicPointView(ModelViewSet):
     # queryset = TuristicPoint.objects.all()
     serializer_class = TuristicPointSerializer
     def get_queryset(self):
-        return TuristicPoint.objects.filter(approval = True)
+        
+        try:
+            queryset = TuristicPoint.objects.all()
+            id = self.request.query_params.get("id", None)
+            name_params = self.request.query_params.get("name", None)
+        
+            if id:
+                queryset = TuristicPoint.objects.filter(pk=id)
+            if name_params:
+                queryset = TuristicPoint.objects.filter(name__iexact = name_params)
+            return queryset 
+
+        except:
+            return TuristicPoint.objects.filter(approval = True) 
+
 
     def list(self, request, *args, **kwargs):
         try:
+            return super(TuristicPointView, self).list(request, *args, **kwargs) 
+        except:
             date = requests.get("https://cdn.apicep.com/file/apicep/59650-000.json")
             date_json = json.loads(date.content)
-            print(date_json)
+            
             return Response(date_json)
-        except:
-            return Response({"Ola": "Passei direto"})
-            # super(TuristicPointView, self).list(request, *args, **kwargs)
+            
 
 
 
@@ -44,6 +58,6 @@ class TuristicPointView(ModelViewSet):
     def denunciar(self, request, pk=None):
         return Response({"teste": "Cheguei na action"})
 
-    @action(methods=["post", "get"], detail=True)
+    @action(methods=["get"], detail=True)
     def test(self, request, pk=None):
         return Response({"teste": "Cheguei na action"})
